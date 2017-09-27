bf4207242c5a9a5922cd88915b6b3e7e437da3b6
Udit Agrawal <uditagrawal95@gmail.com>
2017-09-27 15:07:50 +0530

    initial commit

---
+++ src/main/java/com/airtel/app/vo/CalcInput.java
@@ -0,0 +1,25 @@
+package com.airtel.app.vo;
+
+public class CalcInput {
+
+	private int num1;
+    private int num2;
+	public int getNum1() {
+		return num1;
+	}
+	public void setNum1(int num1) {
+		this.num1 = num1;
+	}
+	public int getNum2() {
+		return num2;
+	}
+	public void setNum2(int num2) {
+		this.num2 = num2;
+	}
+	@Override
+	public String toString() {
+		return "CalcInput [num1=" + num1 + ", num2=" + num2 + "]";
+	}
+    
+    
+}
---
+++ src/main/java/com/airtel/app/vo/Result.java
@@ -0,0 +1,25 @@
+package com.airtel.app.vo;
+
+public class Result {
+
+	private String result;
+	
+	public Result(String result) {
+        this.result = result;
+    }
+
+	public String getResult() {
+		return result;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+
+	@Override
+	public String toString() {
+		return "Result [result=" + result + "]";
+	}
+	
+	
+}
